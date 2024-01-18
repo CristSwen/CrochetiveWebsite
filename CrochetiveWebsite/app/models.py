1b9e4d6646eb0cd40910ee7b627060b708d133af
CristSwen <123016113+CristSwen@users.noreply.github.com>
2024-01-18 13:21:22 -0700

    Moved the closing body tag from under the footer to above the footer

--- CrochetiveWebsite/app/models.py
+++ CrochetiveWebsite/app/models.py
@@ -10,3 +10,9 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class FeaturedItem(models.Model):
+    image = models.ImageField(upload_to='featured_items/')
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
